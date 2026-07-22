f7bbeef7899821e9578c2380c6a9e8fa25c1ca4c
IbbadArshadButt <ibbadbutt3521@gmail.com>
2026-07-22 18:09:02 +0500

    Add: Added print() method for LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -29,8 +29,27 @@ class LinkedList {
         }
         this.size++;
     }
+
+    print() {
+        if(this.isEmpty()) {
+            console.log('List is empty');
+        } else {
+            let current = this.head;
+            let listvalues = '';
+            while(current) {
+                listvalues += current.value + ' ';
+                current = current.next;
+            }
+            console.log(listvalues);
+        }
+    }
 }
 
 const list = new LinkedList();
 console.log('List is empty:', list.isEmpty()); // true
 console.log('List size:', list.getSize()); // 0
+
+list.prepend(10);
+list.prepend(20);
+list.prepend(30);
+list.print(); // 30 20 10
