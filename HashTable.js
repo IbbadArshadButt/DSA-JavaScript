9eeafc6ea2c973c7ec68eb96a9d027af848f4387
IbbadArshadButt <ibbadbutt3521@gmail.com>
2026-07-29 17:21:41 +0500

    Add: Added remove method for Hashtable

--- HashTable.js
+++ HashTable.js
@@ -21,6 +21,13 @@ class HashTable {
         const index = this.hash(key);
         return this.table[index];
     }
+
+    remove(key) {
+        const index = this.hash(key);
+        this.table[index] = undefined;
+    }
+
+    
 }
 
 
